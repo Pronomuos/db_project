67be2fe63405bc9fe9bac8012c839554b65c23a1
Pronomuos <WizardPronomuos@yandex.ru>
2021-04-07 17:02:21 +0300

    OfNullable added

--- src/main/java/com/itmo/java/basics/logic/impl/DatabaseImpl.java
+++ src/main/java/com/itmo/java/basics/logic/impl/DatabaseImpl.java
@@ -47,21 +47,21 @@ public class DatabaseImpl implements Database {
 
     @Override
     public void write(String tableName, String objectKey, byte[] objectValue) throws DatabaseException {
-        var table = Optional.of(tables.get(tableName)).
+        var table = Optional.ofNullable(tables.get(tableName)).
                 orElseThrow(() -> new DatabaseException("Cannot find the table, called - " + tableName + "."));
         table.write(objectKey, objectValue);
     }
 
     @Override
     public Optional<byte[]> read(String tableName, String objectKey) throws DatabaseException {
-        var table = Optional.of(tables.get(tableName)).
+        var table = Optional.ofNullable(tables.get(tableName)).
                 orElseThrow(() -> new DatabaseException("Cannot find the table, called - " + tableName + "."));
         return table.read(objectKey);
     }
 
     @Override
     public void delete(String tableName, String objectKey) throws DatabaseException {
-        var table = Optional.of(tables.get(tableName)).
+        var table = Optional.ofNullable(tables.get(tableName)).
                 orElseThrow(() -> new DatabaseException("Cannot find the table, called - " + tableName + "."));
         table.delete(objectKey);
     }
